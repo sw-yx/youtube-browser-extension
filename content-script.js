3bfe40b577716354eca5e7a7b74356f1ab129ca0
swyx <shawnthe1@gmail.com>
2022-10-06 04:09:27 +0200

    readme

--- content-script.js
+++ content-script.js
@@ -14,8 +14,8 @@ var { origin, pathname, host } = window.location;
 var SortMenuEl = document.getElementById("sort-menu")
 var isOnChannel = document.getElementById("channel-container")
 console.log({ isOnChannel })
-console.log('SortMenuEl.children.length < 1', SortMenuEl.children.length < 1)
-if (isOnChannel && SortMenuEl.children.length < 1) {
+console.log('SortMenuEl.children.length < 1', SortMenuEl?.children?.length < 1)
+if (isOnChannel && SortMenuEl?.children?.length < 1) {
   const urls = {
     popular: `?view=0&sort=p&flow=grid`,
     oldest: `?view=0&sort=da&flow=grid`,
@@ -23,10 +23,10 @@ if (isOnChannel && SortMenuEl.children.length < 1) {
   }
   const loc = origin + pathname
   SortMenuEl.innerHTML = `
-  <ul>
-    <li><a style-target="label-text" class="style-scope yt-dropdown-menu" href="${loc + urls.popular}">Popular</a></li>
-    <li><a style-target="label-text" class="style-scope yt-dropdown-menu" href="${loc + urls.oldest}">Oldest</a></li>
-    <li><a style-target="label-text" class="style-scope yt-dropdown-menu" href="${loc + urls.newest}">Newest</a></li>
+  <ul style="display: flex; gap: 3rem">
+    <li><a style="font-size: initial;" style-target="label-text" class="style-scope yt-dropdown-menu" href="${loc + urls.popular}">Popular</a></li>
+    <li><a style="font-size: initial;" style-target="label-text" class="style-scope yt-dropdown-menu" href="${loc + urls.oldest}">Oldest</a></li>
+    <li><a style="font-size: initial;" style-target="label-text" class="style-scope yt-dropdown-menu" href="${loc + urls.newest}">Newest</a></li>
   </ul>
   `
 }
